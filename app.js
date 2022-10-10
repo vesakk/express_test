9b08fdf032106d7d200450a12783fe9fed2d9362
Vesa Kauppinen <vesa.kauppinen@hiq.fi>
2022-10-10 13:02:34 +0300

    ...

---
+++ app.js
@@ -0,0 +1,21 @@
+const Redis = require("redis");
+
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const redis = Redis.createClient({
+  host: "ip-10-0-17-15.ec2.internal",
+  db: 0,
+  port: 6379,
+});
+
+redis.on("error", (err) => console.log("Redis Client Error", err));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
