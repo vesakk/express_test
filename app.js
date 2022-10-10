6143dc8ccd15ae9f30c2a4855f11e0ccc32d46b1
Vesa Kauppinen <vesa.kauppinen@hiq.fi>
2022-10-10 13:25:46 +0300

    ...

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
-const Redis = require("redis");
+import { createClient } from 'redis';
 
-const express = require("express");
-const app = express();
-const port = 3000;
-
-const redis = Redis.createClient({
+const client = createClient({
   host: "ip-10-0-17-15.ec2.internal",
   db: 0,
   port: 6379,
 });
 
+client.on('error', (err) => console.log('Redis Client Error', err));
 
-redis.hSet("tests:eka", [
-    "active", 1,
-    "show", 2
-])
+await client.connect();
 
-redis.on("error", (err) => console.log("Redis Client Error", err));
+await client.set('key', 'value');
+const value = await client.get('key');
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
