9d1c29f58de23a72b01edad52287315b9cd9400d
Vesa Kauppinen <vesa.kauppinen@hiq.fi>
2022-10-10 13:41:03 +0300

    ....

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-import { createClient } from 'redis';
+import { createClient } from "redis";
 
 const client = createClient({
-  host: "ip-10-0-17-15.ec2.internal",
-  db: 0,
-  port: 6379,
+  url: "redis://alice:foobared@aip-10-0-17-15.ec2.internal:6379",
 });
 
-client.on('error', (err) => console.log('Redis Client Error', err));
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 await client.connect();
 
-await client.set('key', 'value');
-const value = await client.get('key');
-
+await client.set("key", "value");
+const value = await client.get("key");
