ef55ff3e28c3e1bbdf24fdeb74037eceed076ebf
Vesa Kauppinen <vesa@residentia.fi>
2022-10-14 12:41:30 +0300

    ....

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+require('dotenv').config({
+  path: './.env'
+})
+
 const express = require('express')
 const mysql = require('mysql2/promise')
+const bluebird = require('bluebird');
 
 const app = express()
 const port = 3000
 
+
+// create the connection, specify bluebird as Promise
+const connection = await mysql.createConnection({
+  host: process.env.DB_ENDPOINT,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USERNAME,
+  database: process.env.DB_NAME,
+  Promise: bluebird
+});
 
 
 
